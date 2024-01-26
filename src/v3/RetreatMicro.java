fc8d58f3fac35284c3e7c85071abb447423dd4be
Reece Yang <reeceyang@live.com>
2024-01-25 20:36:38 -0500

    feat: create v3 and healing and retreat micro

---
+++ src/v3/RetreatMicro.java
@@ -0,0 +1,17 @@
+package v3;
+
+import battlecode.common.*;
+
+class RetreatMicro {
+    public static void doRetreat(RobotController rc) throws GameActionException {
+        MapLocation[] spawnLocs = rc.getAllySpawnLocations();
+        MapLocation closestSpawnLoc = spawnLocs[0];
+        for (int i = 1; i < spawnLocs.length; i++) {
+            if (rc.getLocation().distanceSquaredTo(spawnLocs[i]) < rc.getLocation().distanceSquaredTo(closestSpawnLoc)) {
+                closestSpawnLoc = spawnLocs[i];
+            }
+        }
+        Pathing.moveTowards(rc, closestSpawnLoc);
+        CombatMicro.doTryShoot(rc); // attempt to move-shoot
+    }
+}
